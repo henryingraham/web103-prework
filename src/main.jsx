4b0c7e151e0a67c9a8dd193f541a1f18b672895a
henryingraham <hingraham@ufl.edu>
2023-08-22 03:13:49 -0400

    package json

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
 import AddCreator from './pages/AddCreator';
